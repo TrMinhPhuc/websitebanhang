879ccdf872d6898a4ac363427b38f301487326f5
Phuc <Phuc@TRANMINHPHUC>
2022-11-18 22:09:16 +0700

    Add project files.

---
+++ websitebanhang/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace websitebanhang.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
